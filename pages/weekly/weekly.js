6378567c2aae7f828223269a6d8dd8cf69c4b4b3
FesonX <fesonx@foxmail.com>
2018-10-30 11:39:17 +0800

    [Remove]Weekly Pages' Hard Code

--- pages/weekly/weekly.js
+++ pages/weekly/weekly.js
@@ -1,1 +1,11 @@
-Page({})
+Page({
+  data:{
+    thisWeekMovie:{
+      name: "网络谜踪",
+      comment: "桌面电影新高度",
+      imagePath: "/images/searching.jpg",
+      score:"8.7",
+      isHighlyRecommended: false
+    }
+  }
+})
